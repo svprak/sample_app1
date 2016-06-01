585e9976cb81b56a03bf31cd6fbd9e6d75b077f8
svp <svprak@gmail.com>
2016-06-01 02:11:08 +0000

    some update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,17 @@
 class UsersController < ApplicationController
   before_action :logged_in_user, only: [:edit, :update]
   before_action :correct_user, only: [:edit, :update]
+  
   def show
     @user = User.find(params[:id])
   end
+  
+  
   def new
     @user = User.new
   end
+  
+  
   def create
     @user = User.new(user_params)
     if @user.save
@@ -17,9 +22,13 @@ class UsersController < ApplicationController
       render 'new'
     end
   end
+  
+  
   def edit
     @user = User.find(params[:id])
   end
+  
+  
   def update
     @user = User.find(params[:id])
     if @user.update_attributes(user_params)
@@ -35,6 +44,8 @@ class UsersController < ApplicationController
     def user_params
       params.require(:user).permit(:name, :email, :password, :password_confirmation)
     end
+    
+    
     def logged_in_user
       unless logged_in?
         store_location
@@ -42,6 +53,8 @@ class UsersController < ApplicationController
         redirect_to login_url
       end
     end
+    
+    
     def correct_user
       @user = User.find(params[:id])
       redirect_to(root_url) unless @user == current_user
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -12,8 +12,8 @@ module SessionsHelper
   end
 
   # Returns the user corresponding to the remember token cookie.
-  def current_user?(user)
-    user == current_user
+  def current_user
+   # user == current_user
     if (user_id = session[:user_id])
       @current_user ||= User.find_by(id: user_id)
     elsif (user_id = cookies.signed[:user_id])
@@ -29,22 +29,26 @@ module SessionsHelper
   def logged_in?
     !current_user.nil?
   end
+  
   # forget a persistent sessionde
   def forget(user)
     user.forget
     cookies.delete(:user_id)
     cookies.delete(:remember_token)
   end
+  
   # Logs out the current user.
   def log_out
     forget(current_user)
     session.delete(:user_id)
     @current_user = nil
   end
+  
   def redirect_back_or(default)
     redirect_to(session[:forwarding_url] || default)
     session.delete(:forwarding_url)
   end
+  
   def store_location
     session[:forwarding_url] = request.url if request.get?
   end
